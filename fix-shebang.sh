76f88bd511b8f65a8e90ca2330c1c9ef54f8a8ec
mrgofuckyourself <anarchynjw@gmail.com>
2021-09-21 17:14:19 -0700

    Initial commit

---
+++ fix-shebang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Which directory should I search to update old shell shebang strings in files?"
+read -re "directory"
+echo "Searching and updating files in $directory"
+grep -rIl --word-regexp "#\!\/bin/sh" "$directory" | while read FILE
+do
+	sed -i 's|#\!\/bin/sh|#\!\/bin/bash|' $FILE
+done
