06408662dd2ac14625dd120601c5fe24b62653dc
mrgofuckyourself <anarchynjw@gmail.com>
2021-12-09 02:57:19 -0800

    commit

--- fix-shebang.sh
+++ fix-shebang.sh
@@ -3,7 +3,7 @@
 echo "Which directory should I search to update old shell shebang's in?"
 read -re "directory"
 echo "Searching and updating files in $directory"
-grep -rIl --word-regexp "#\!\/bin/sh" "$directory" | while read FILE
+grep -rIl --word-regexp "/bin/sh" "$directory" | while read FILE
 do
-	sed -i 's|#\!\/bin/sh|#\!\/bin/bash|' $FILE
+	sed -i 's|/bin/sh|/bin/bash|' $FILE
 done
